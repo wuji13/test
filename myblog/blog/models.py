a61313cb8c014e58e89db1e4441e55a42af6078a
刘运强 <liuyunqiang@yunjinkj.com>
2017-06-07 14:49:23 +0800

    models：数据模型；templates:模板的编写

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class article(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField(null=True)
+
--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/', views.index),
+    url(r'^index/$', views.index),
 ]
