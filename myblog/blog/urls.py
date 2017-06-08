0a648e8d5e2fa5a1de302fec73f900a2e38e0c99
刘运强 <liuyunqiang@yunjinkj.com>
2017-06-08 15:45:51 +0800

    可以新增，修改博客文章了

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
-    url(r'^article/(?P<article_id>[0-9]+)$', views.article,name='article_page'),
-    url(r'^edit/$', views.edit),
-    url(r'^edit/action$', views.edit_submit,name='article_action'),
+    url(r'^index/', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)', views.article,name='article_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)', views.edit_page,name='edit_page'),
+    url(r'^edit/action', views.edit_submit,name='article_action'),
+
+
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -12,18 +12,34 @@ def index(request):
 	return  render(request,'blog/index.html',{'a':articles})
 
 def article(request,article_id):
+	print(article_id)
 	art = models.article.objects.get(pk=article_id)
 	return  render(request,'blog/article_page.html',{'b':art})
 
-def edit(request):
-	return  render(request,'blog/edit_page.html')
+def edit_page(request,article_id):
+	if str(article_id)=='0':
+		return render(request, 'blog/edit_page.html')
+	art = models.article.objects.get(pk=article_id)
+	return render(request,'blog/edit_page.html',{'c':art})
 
 def edit_submit(request):
-	title=request.POST.get('title','TITLE')
-	content=request.POST.get('content','content')
-	models.article.objects.create(title=title,content=content)
-	articles = models.article.objects.all
-	return render(request, 'blog/index.html', {'a': articles})
+	title = request.POST.get('title', 'TITLE')
+	content = request.POST.get('content', 'content')
+	article_id = request.POST.get('id', 'id')
+	if article_id=='0':
+	  models.article.objects.create(title=title,content=content)
+	  articles = models.article.objects.all
+	  return render(request, 'blog/index.html', {'a': articles})
+	else:
+		a=models.article.objects.get(pk=article_id)
+		a.title=title
+		a.content=content
+		a.save()
+		return render(request, 'blog/article_page.html', {'b':a})
+
+
+
+
 
 
 
