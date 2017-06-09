b6c399b598443fd392f35af8bc0cd10cb158277b
刘运强 <liuyunqiang@yunjinkj.com>
2017-06-09 15:04:34 +0800

    no message

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -5,5 +5,10 @@ from django.contrib import admin
 
 from .models import article
 
+
+class articleAdmin(admin.ModelAdmin):
+    list_display=('id','title','content')
+    #过滤器，起筛选作用
+    list_filter=('id',)
 #在admin中注册models
-admin.site.register(article)
+admin.site.register(article,articleAdmin)
