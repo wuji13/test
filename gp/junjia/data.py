c3b76dc5dfb403bf8809e1547b20aa38ceea0af1
ji wu <753217919@qq.com>
2017-08-12 19:30:40 +0800

    11

--- gp/junjia/data.py
+++ gp/junjia/data.py
@@ -2,21 +2,19 @@
 
 import tushare as ts 
 from draw import draw  
+import numpy as np  
+import matplotlib.pyplot as plt  
 
 df = ts.get_h_data('002457', autype=None) #不复权
 
 v = df.volume
 a = df.amount
-
-s = []
-d = []
-x=len(a)
-for i in range(0,x):
-	p = round(a[i]/v[i],1)
-	s.append(p)
-	d.append(i)
-
-
-print(s[1])
-print(type(s))
-draw(d,s)
+close = df.close
+price = a/v
+f = df['close'].plot()
+p = price.plot()
+plt.xlabel("时间")  
+	#y轴名称
+plt.ylabel("均价")   
+plt.show(f)
+plt.show(p)
